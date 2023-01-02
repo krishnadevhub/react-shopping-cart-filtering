eeea43417d9edf9da0921eb309d1539efda17ebe
Krishna <krishna941@hotmail.com>
2023-01-02 18:45:41 +0800

    Create React Context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-//import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from 'react';
 import {
   Badge,
-  CarouselItem,
   Container,
   Dropdown,
   FormControl,
   Nav,
   Navbar
-} from 'react-bootstrap'
+} from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Navbar bg='dark' variant='dark' style={{ height: 80 }}>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a href='/'>Shopping Cart</a>
+          <Link to='/'>Shopping Cart</Link>
         </Navbar.Brand>
-        <Navbar.Text className='search'>
+        <Navbar.Text className="search">
           <FormControl
             style={{ width: 500 }}
             placeholder="Search a product"
-            className='m-auto'
+            className="m-auto"
           />
         </Navbar.Text>
         <Nav>
-          <Dropdown alignRight>
-            <Dropdown.Toggle variant='success'>
-              {/* <FaShoppingCart color='white' fontSize='25px' /> */}
+          <Dropdown align="end">
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
               <Badge>{10}</Badge>
             </Dropdown.Toggle>
             <Dropdown.Menu style={{ minWidth: 370 }}>

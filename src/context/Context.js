1e82460025f95e5b3531776e4f799a6f703db3f7
Krishna <krishna941@hotmail.com>
2023-06-12 21:51:33 +0800

    Add Context

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,9 +1,29 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { faker } from '@faker-js/faker';
+
 
 const Cart = createContext();
 
 const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({  
+    id: faker.datatype.uuid(),  
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.business(),    
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),    
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+ 
   return <Cart.Provider>{children}</Cart.Provider>
+
+  
 };
 
 export default Context;
